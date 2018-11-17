4f00f033010bd6d3f1ae51d9fdf8688b8a8197df
Donya Hojabr <donya.hojabr@mail.mcgill.ca>
2018-11-17 09:08:16 -0500

    added vue.js web frontend

---
+++ CodeJam-Web/src/components/Test.vue
@@ -0,0 +1,34 @@
+<template>
+ <div id="tester">
+    <h2>Participants</h2>
+    <table>
+      <tr>
+          <td>John</td>
+          <td>Event to attend</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Participant Name">
+          </td>
+          <td>
+              <button>Create</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+<script>
+  // TODO
+</script>
+
+<style>
+  #tester {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
