2ad110e269883e6ee8f3bbaf9bafcfb1c77c99de
Donya Hojabr <donya.hojabr@mail.mcgill.ca>
2018-11-17 10:36:05 -0500

    added controller

---
+++ src/main/java/ca/mcgill/codejam/controller/TestController.java
@@ -0,0 +1,26 @@
+package ca.mcgill.codejam;
+
+import ca.mcgill.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.web.bind.annotation.*;
+import org.hibernate.Hibernate;
+import org.hibernate.SQLQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Test")
+public class TestController {
+
+ @RequestMapping("/helloVue")
+ public String helloVue(@RequestParam(value="name") String name) {
+
+ String result = "Hello" + name;
+
+ return result;
+ }
+
+}
