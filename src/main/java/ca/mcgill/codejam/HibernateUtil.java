3d80f0c6780f9c7a431e4d04d0ad428b7ea279e8
cattanisam <samantha.cattani@mail.mcgill.ca>
2018-11-16 21:42:04 -0500

    bare bones

---
+++ src/main/java/ca/mcgill/codejam/HibernateUtil.java
@@ -0,0 +1,27 @@
+package ca.mcgill.codejam;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+    
+    static void createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static Session getSession() {
+        if (sessionFactory == null) {
+            createSessionFactory();
+        }
+        return sessionFactory.openSession();
+    }
+}
